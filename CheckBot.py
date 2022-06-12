9daed4f13cd4d717c81b1f28d67e6f374d552f1e
Artur <artur_goncharov_97@mail.ru>
2022-06-12 18:38:31 +0300

    Getting checks using the Long polling mechanism

--- CheckBot.py
+++ CheckBot.py
@@ -1,4 +1,5 @@
 import os
+import time
 from typing import NoReturn
 import requests
 
@@ -6,5 +7,24 @@ TOKEN = os.environ.get("TOKEN")
 
 
 def get_checks() -> NoReturn:
-    request=requests.get("") 
-    
+    url = "https://dvmn.org/api/long_polling/"
+    headers = {
+        "Authorization": f"Token {TOKEN}"
+        }
+    timestamp = time.time()
+    while True:
+        print(timestamp)
+        try:
+            request = requests.get(url, headers=headers, params=str(timestamp))
+            request.raise_for_status()
+            request = request.json()
+            if request['status'] == "timeout":
+                timestamp = request['timestamp_to_request']
+        except requests.exceptions.ReadTimeout:
+            print("Timeout exceeded")
+        except requests.exceptions.ConnectionError:
+            print("Connection Error")
+        print(request)
+
+
+get_checks()
