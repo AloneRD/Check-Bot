9e3cab7696bf7e3f3d8dbafaee76e784b96b3144
Artur <artur_goncharov_97@mail.ru>
2022-06-15 21:59:09 +0300

    Changed naming. Added if/name/main.Add env CHAT_ID

--- CheckBot.py
+++ CheckBot.py
@@ -6,42 +6,43 @@ import requests
 import telegram
 from dotenv import load_dotenv
 
-load_dotenv()
-
-TOKEN = os.getenv("TOKEN")
-CHAT_ID = os.environ.get("CHAT_ID")
-
-bot = telegram.Bot("5358565467:AAHamFQOxdBci_scJkFHxJ6cUvo_81El5Yc")
-
 
 def get_checks() -> NoReturn:
-    url = "https://dvmn.org/api/long_polling/"
+    AUTH_TOKEN_API = os.getenv("AUTH_TOKEN")
+
+    url_api = "https://dvmn.org/api/long_polling/"
     headers = {
-        "Authorization": f"Token {TOKEN}"
+        "Authorization": f"Token {AUTH_TOKEN_API}"
         }
     timestamp = time.time()
     while True:
         try:
-            request = requests.get(url, headers=headers, params=str(timestamp))
-            request.raise_for_status()
-            request = request.json()
-            if request['status'] == "timeout":
-                timestamp = request['timestamp_to_request']
+            response_tasks = requests.get(url_api, headers=headers, params=str(timestamp), timeout=20)
+            response_tasks.raise_for_status()
+            response_tasks = response_tasks.json()
+            if response_tasks['status'] == "timeout":
+                timestamp = response_tasks['timestamp_to_request']
             else:
-                send_message_telegramm(request)
+                timestamp = time.time()
+                send_message_telegramm(response_tasks)
         except requests.exceptions.ReadTimeout:
-            print("Timeout exceeded")
+            pass
         except requests.exceptions.ConnectionError:
-            print("Connection Error")
+            time.sleep(5)
 
 
 def send_message_telegramm(task: json) -> NoReturn:
+    CHAT_ID = os.environ.get("TG_CHAT_ID")
+    TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+    bot = telegram.Bot(TELEGRAM_TOKEN)
+
     tasks = task['new_attempts']
     for task in tasks:
         lesson_title = task['lesson_title']
         lesson_url = task['lesson_url']
         result = task['is_negative']
-        if result is True:
+        if result:
             text_message = f"У Вас проверили работу {lesson_title}.\n\r\
                              Ссылка на работу {lesson_url}.\n\r\
                              К сожалению в работе нашлись ошибки."
@@ -49,7 +50,9 @@ def send_message_telegramm(task: json) -> NoReturn:
             text_message = f"У Вас проверили работу {lesson_title}.\n\r\
                              Ссылка на работу {lesson_url}\n\r\
                              Преподователю все понравилось!"
-        bot.send_message(chat_id=937205408, text=text_message)
+        bot.send_message(chat_id=CHAT_ID, text=text_message)
 
 
-get_checks()
+if __name__ == "__main__":
+    load_dotenv()
+    get_checks()
