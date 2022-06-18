b9eb231c59a095654d161de22931f1d331720dcc
Artur <artur_goncharov_97@mail.ru>
2022-06-18 15:57:34 +0300

    catch errors and write to log

--- CheckBot.py
+++ CheckBot.py
@@ -1,23 +1,25 @@
+from asyncio.log import logger
 import json
+import logging
 import os
 import time
 from typing import NoReturn
 import requests
 import telegram
-from dotenv import load_dotenv
-import logging
+from handler import TelegramLogsHandler
 
 logger = logging.getLogger("CheckBot")
 logger.setLevel(logging.INFO)
+logger.addHandler(TelegramLogsHandler())
+
 
 
 def main():
-    load_dotenv()
     auth_token_api = os.getenv("AUTH_TOKEN")
     chat_id = os.environ.get("TG_CHAT_ID")
     token = os.getenv("TELEGRAM_TOKEN")
 
-    logger.warning("Бот запущен")
+    logger.info("Бот запущен")
 
     get_checks(auth_token_api, chat_id, token)
 
@@ -37,7 +39,7 @@ def get_checks(auth_token_api: str, chat_id: str, token: str) -> NoReturn:
                 timestamp = review_info['timestamp_to_request']
             else:
                 timestamp = review_info["last_attempt_timestamp"]
-                send_message_telegramm(review_info, chat_id, token) 
+                send_message_telegramm(review_info, chat_id, token)
         except requests.exceptions.ReadTimeout:
             pass
         except requests.exceptions.ConnectionError:
@@ -45,22 +47,25 @@ def get_checks(auth_token_api: str, chat_id: str, token: str) -> NoReturn:
 
 
 def send_message_telegramm(task: json, chat_id: str, token: str) -> NoReturn:
-    bot = telegram.Bot(token)
-
-    tasks = task['new_attempts']
-    for task in tasks:
-        lesson_title = task['lesson_title']
-        lesson_url = task['lesson_url']
-        result = task['is_negative']
-        if result:
-            text_message = f"У Вас проверили работу {lesson_title}.\n\r\
-                             Ссылка на работу {lesson_url}.\n\r\
-                             К сожалению в работе нашлись ошибки."
-        else:
-            text_message = f"У Вас проверили работу {lesson_title}.\n\r\
-                             Ссылка на работу {lesson_url}\n\r\
-                             Преподователю все понравилось!"
-        bot.send_message(chat_id=chat_id, text=text_message)
+    try:
+        bot = telegram.Bot(token)
+        tasks = task['new_attempts']
+        for task in tasks:
+            lesson_title = task['lesson_title']
+            lesson_url = task['lesson_url']
+            result = task['is_negative']
+            if result:
+                text_message = f"У Вас проверили работу {lesson_title}.\n\r\
+                                Ссылка на работу {lesson_url}.\n\r\
+                                К сожалению в работе нашлись ошибки."
+            else:
+                text_message = f"У Вас проверили работу {lesson_title}.\n\r\
+                                Ссылка на работу {lesson_url}\n\r\
+                                Преподователю все понравилось!"
+            bot.send_message(chat_id=chat_id, text=text_message)
+    except Exception as err:
+        logger.warning("Бот упал с ошибкой")
+        logger.error(err, exc_info=True)
 
 
 if __name__ == "__main__":
